00aa4f225deae94c5d586a39caf4365f07cceef1
elik kozak <elik.kozak@gmail.com>
2019-12-07 21:06:52 +0200

    1

--- src/test/java/bgu/spl/mics/SquadTest.java
+++ src/test/java/bgu/spl/mics/SquadTest.java
@@ -1,14 +1,20 @@
 package bgu.spl.mics;
 
+import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class SquadTest {
+    private Squad squad;
     @BeforeEach
     public void setUp(){
+        squad = Squad.getInstance();
+        Gson gson = new Gson();
 
+        String[] squad = gson.fromJson("[\"Squad\"]", String[].class);
+        squad.load(squad);
     }
 
     @Test
