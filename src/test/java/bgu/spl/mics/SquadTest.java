3cf37174f84ecc28cc0bde2868edb43c24303d0a
elik kozak <elik.kozak@gmail.com>
2019-12-10 21:40:52 +0200

    1

--- src/test/java/bgu/spl/mics/SquadTest.java
+++ src/test/java/bgu/spl/mics/SquadTest.java
@@ -1,8 +1,20 @@
 package bgu.spl.mics;
 
+import bgu.spl.mics.application.passiveObjects.Agent;
 import com.google.gson.Gson;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -11,15 +23,36 @@ public class SquadTest {
     @BeforeEach
     public void setUp(){
         squad = Squad.getInstance();
-        Gson gson = new Gson();
+        HashMap<String, Agent> agents=new HashMap<String, Agent>()  ;
+        JSONParser jsonParser = new JSONParser();
+        try{
+            Object obj = jsonParser.parse(new FileReader("input201.json"));
+            JSONObject jsonObject= (JSONObject) obj;
+            JSONArray squadArray=(JSONArray) jsonObject.get("squad");
+            squadArray.forEach( squad -> parseSquadObject( (JSONObject) squad, agents ) );
 
-        String[] squad = gson.fromJson("[\"Squad\"]", String[].class);
-        squad.load(squad);
-    }
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    @Test
+        squad.load(agents);
+        @Test
     public void test(){
         //TODO: change this test and add more tests :)
         fail("Not a good test");
     }
+}
+
+    private void parseSquadObject(JSONObject squad,HashMap<String,Agent> agents) {
+        String name=(String) squad.get("name");
+        String serialNumber=(String) squad.get("serialNumber");
+        agents.put(serialNumber,new Agent(serialNumber,name));
+    }
+
+
 }
