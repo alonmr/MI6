0e7031825bf75372ee5472d8123bbff1edc40b0e
elik kozak <elik.kozak@gmail.com>
2019-12-15 14:14:52 +0200

    1

--- src/test/java/bgu/spl/mics/MessageBrokerTest.java
+++ src/test/java/bgu/spl/mics/MessageBrokerTest.java
@@ -4,20 +4,35 @@ import bgu.spl.mics.application.subscribers.M;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MessageBrokerTest {
-    private MessageBroker MB;
+    private MessageBroker massageBrokerTest;
     @BeforeEach
     public void setUp(){
-        MB = MessageBrokerImpl.getInstance();
-        M M = new M();
-        MB.register(M);
+        massageBrokerTest = MessageBrokerImpl.getInstance();
+
+        massageBrokerTest.subscribeEvent(MissionReceivedEvent(mission));
     }
 
     @Test
     public void test(){
-
+        boolean thrown=false;
+        M M1 = new M();
+        M M2 = new M();
+        massageBrokerTest.register(M1);
+        try{
+            massageBrokerTest.awaitMessage(M1);
+        } catch (InterruptedException e) {
+            thrown=true;
+        }
+        assertFalse(thrown);
+        try{
+            massageBrokerTest.awaitMessage(M2);
+        } catch (InterruptedException e) {
+            thrown=true;
+        }
+        assertTrue(thrown);
 
     }
 }
